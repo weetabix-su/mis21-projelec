03432ae6863d164f18e4b266ba6934f21a3ee93d
Mikko Luis Saavedra <vovo27_miranemiko@yahoo.co.jp>
2015-07-15 11:25:23 +0800

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get 'pages/candidates'
 
+  root to: "pages#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
